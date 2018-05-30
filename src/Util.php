f8162320cd649a479ca5bd554c76762677c63c2b
sc0Vu <alk03073135@gmail.com>
2018-05-30 16:34:02 +0800

    Add sha3

---
+++ src/Util.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * This file is part of ethereum-util package.
+ * 
+ * (c) Kuan-Cheng,Lai <alk03073135@gmail.com>
+ * 
+ * @author Peter Lai <alk03073135@gmail.com>
+ * @license MIT
+ */
+
+namespace Web3p\EthereumUtil;
+
+use InvalidArgumentException;
+use RuntimeException;
+use kornrunner\Keccak;
+use Elliptic\EC;
+use Elliptic\EC\KeyPair;
+
+class Util
+{
+    /**
+     * SHA3_NULL_HASH
+     * 
+     * @const string
+     */
+    const SHA3_NULL_HASH = 'c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470';
+
+    /**
+     * secp256k1
+     * 
+     * @var \Elliptic\EC
+     */
+    protected $secp256k1;
+
+    /**
+     * construct
+     * 
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->secp256k1 = new EC('secp256k1');
+    }
+
+    /**
+     * get
+     * 
+     * @param string $name
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        $method = 'get' . ucfirst($name);
+
+        if (method_exists($this, $method)) {
+            return call_user_func_array([$this, $method], []);
+        }
+        return false;
+    }
+
+    /**
+     * set
+     * 
+     * @param string $name
+     * @param mixed $value
+     * @return mixed
+     */
+    public function __set($name, $value)
+    {
+        $method = 'set' . ucfirst($name);
+
+        if (method_exists($this, $method)) {
+            return call_user_func_array([$this, $method], [$value]);
+        }
+        return false;
+    }
+
+    /**
+     * sha3
+     * keccak256
+     * 
+     * @param string $value
+     * @return string
+     */
+    public function sha3(string $value)
+    {
+        $hash = Keccak::hash($value, 256);
+
+        if ($hash === $this::SHA3_NULL_HASH) {
+            return null;
+        }
+        return $hash;
+    }
+}
---
+++ test/unit/UtilTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Test\Unit;
+
+use Test\TestCase;
+use Web3p\EthereumUtil\Util;
+
+class UtilTest extends TestCase
+{
+    /**
+     * testSha3
+     * 
+     * @return void
+     */
+    public function testSha3()
+    {
+        $util = new Util;
+
+        $this->assertNull($util->sha3(''));
+        $this->assertEquals('47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad', $util->sha3('hello world'));
+    }
+}
